21b44f386b038b42b411151df8a99195c1d5a163
Ayush jain <ayushjain@Ayushs-MacBook-Air.local>
2025-11-01 23:07:22 +0530

    Style fixes - detail page

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -73,7 +73,7 @@ export default function MovieDetailPage() {
         </div>
 
         {/* Content Section */}
-        <div className="container mx-auto px-4 -mt-32 relative z-10">
+        <div className="container mx-auto px-4 -mt-32 relative z-10 pb-16">
           <div className="flex flex-col md:flex-row gap-8">
             {/* Poster */}
             <div className="flex-shrink-0">
